665ccb24e564de4447bf9073ae123e40ecd745f6
Dexter <dexter.lente18@gmail.com>
2024-08-20 14:43:25 +0800

    added router push

--- src/app/_mainbody/cryptodescription.tsx
+++ src/app/_mainbody/cryptodescription.tsx
@@ -1,11 +1,14 @@
 import useSWR from "swr";
 import { fetcher } from "../_components/ulits/fetcher";
+import { useRouter } from "next/navigation";
 
 export const CryptoDescription = ({ title }: any) => {
   const { data, error } = useSWR(
     `/api/cryptocurrencies/list/${title}`,
     fetcher
   );
+
+  const router = useRouter();
 
   if (error) return <div>Failed to load description</div>;
   if (!data) return <div>Loading...</div>;
@@ -17,6 +20,9 @@ export const CryptoDescription = ({ title }: any) => {
             <div
               key={index}
               className="cursor-pointer hover:underline hover:text-indigo-900"
+              onClick={() =>
+                router.push(`/technical-analysis/${descItem.generate_tracker}`)
+              }
             >
               {descItem.generate_tracker || "No "} Technical Analysis
             </div>
